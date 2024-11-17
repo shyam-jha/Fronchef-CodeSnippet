deff89f89c46718a121f13d83822b5e5e6762fb8
shyam-jha <shyamjhaedu@gmail.com>
2024-11-17 13:24:49 +0530

    first commit

---
+++ component/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white py-6 flex flex-col items-center">
+      {/* Logo */}
+      <img
+        src="/logoo.png"
+        alt="Logo"
+        className="rounded-lg border-[3px] border-gray-500 h-[10rem] mb-2"
+      />
+      {/* Text */}
+      <p className="text-center text-sm">
+        Powered by <span className="font-semibold">@Fronchef</span>  
+      </p>
+      <p className="text-center text-sm mt-1">
+        Component Made with ❤️ by <span className="font-semibold"><a href="https://github.com/shyam-jha" className="underline text-blue">Shyam Jha</a></span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,55 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const colors = require("tailwindcss/colors");
+
+const config: Config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./component/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./data/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: colors.white,
+        black: colors.black,
+        zinc: colors.zinc, // for `bg-zinc-900` and `border-zinc-600`
+        green: {
+          500: "#22c55e", // `text-green-500`
+        },
+      },
+      maxWidth: {
+        "85%": "85%",
+      },
+      height: {
+        auto: "auto",
+        "96": "24rem", // 384px equivalent
+      },
+      maxHeight: {
+        "96": "24rem", // 384px equivalent
+      },
+      borderRadius: {
+        lg: "0.5rem", // Default large radius
+      },
+      padding: {
+        1: "0.25rem",
+        4: "1rem",
+      },
+      fontSize: {
+        lg: "1.125rem", // Text size
+      },
+      spacing: {
+        "1/2": "50%", // For `translate-y-1/2`
+      },
+      transitionTimingFunction: {
+        "transition-all": "all 0.3s ease", // Hover transitions
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
